1e28c21a1eb666f1e857c47b47c13492a2c8c583
Jeevan Bagadi <bagadijeevan@gmail.com>
2024-09-10 19:14:28 -0500

    first commit

--- main.go
+++ main.go
@@ -1,18 +1,7 @@
 package main
-import (
-    "fmt"
-    "os"
-)
+
+import "fmt"
+
 func main() {
-    fmt.Println("Running a custom Drone plugin")
-    // Read environment variables
-    input := os.Getenv("PLUGIN_INPUT")
- 
-    // Do something with the input
-    if input == "" {
-        fmt.Println("No input provided")
-    } else {
-        fmt.Printf("Input received: %s\n", input)
-    }
-    fmt.Println("Plugin execution completed")
+	fmt.Println("Hello From Go...")
 }
