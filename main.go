445e31145a4ab0cb4670c1adecf3a44e73c851b0
Jeevan Bagadi <bagadijeevan@gmail.com>
2024-09-10 23:12:34 -0500

    1.23.0 to 1.23

--- main.go
+++ main.go
@@ -1,18 +1,11 @@
 package main
+
 import (
-    "fmt"
-    "os"
+	"fmt"
 )
+
 func main() {
-    fmt.Println("Running a custom Drone plugin")
-    // Read environment variables
-    input := os.Getenv("PLUGIN_INPUT")
- 
-    // Do something with the input
-    if input == "" {
-        fmt.Println("No input provided")
-    } else {
-        fmt.Printf("Input received: %s\n", input)
-    }
-    fmt.Println("Plugin execution completed")
+	fmt.Println("Running a custom Drone plugin")
+	// Read environment variables
+	fmt.Println("Plugin execution completed")
 }
